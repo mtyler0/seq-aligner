da175d0825c7c5db40143f233f67fd8004e06b46
Michael <mtyler745@gmail.com>
2025-08-29 12:22:42 -0400

    Refactored functions and resolved improper scoring matrix population

--- src/align.py
+++ src/align.py
@@ -4,16 +4,13 @@ from working.helpers import *
 
 
 def main():
-    match, mismatch, gap = 1, -1, -2
     sequence1 = get_fasta_seq("data\\seq1.fasta")
     sequence2 = get_fasta_seq("data\\seq2.fasta")
 
-    ex = Align(match, mismatch, gap)
+    ex = Align()
     print(ex.get_alignment(sequence1, sequence2))
-    print(ex.paths)
-    
-    #print(get_fasta_seq(sequence1))
-    #print(get_fasta_seq(sequence2))
+    print(ex.scoring_matrix)
+
     print("Run executed")
 
 
--- src/working/helpers.py
+++ src/working/helpers.py
@@ -26,30 +26,28 @@ class Align:
     Requires scoring matrix
     """
 
-    def __init__(self, match, mismatch, gap):
+    def __init__(self, match=1, mismatch=-1, gap=-2):
         self.match = match
         self.mismatch = mismatch
         self.gap = gap
 
+
     def get_alignment(self, seq1, seq2):
         # Returns alignment string and score through backtracking
-        alignment_matrix, path_matrix = self.needleman_algo(seq1, seq2)
-        score = alignment_matrix[-1][-1]
-        top = ""
-        matches = ""
-        bottom = ""
+        scoring_matrix, path_matrix = self.needleman_algo(seq1, seq2)
+        score = scoring_matrix[-1][-1]
+        top = "" # Reference strand w/ gaps
+        matches = "" # Visual confirmation of match
+        bottom = "" # Query strand w/ gaps
         i = len(seq1) - 1
         j = len(seq2) - 1
 
         # Backtrack starting at bottom right of matrix and build alignment string based on path score until
         while (i, j) != (-1, -1):
             path = path_matrix[i+1][j+1]
-            paths = []
-            paths.append(path)
-            self.paths = paths
             letter1 = seq1[i]
             letter2 = seq2[j]
-            match = " "
+            match_identifier = " "
 
             if path == 0:
                 i -= 1
@@ -65,32 +63,39 @@ class Align:
 
             top = letter1 + top
             bottom = letter2 + bottom
-            matches = match + matches
+            matches = match_identifier + matches
 
         return f"{top}\n{matches}\n{bottom}\nScore: {score}"
 
-    def needleman_algo(self, seq1, seq2):
+
+    def _initialize_matrices(self, seq1, seq2):
         # Returns scoring matrix based on inputs
         m, n = len(seq1), len(seq2)
-        alignment_matrix = np.zeros((m+1, n+1))
+        scoring_matrix = np.zeros((m+1, n+1))
         path_matrix = np.zeros((m+1, n+1))
 
         # Initialize border rows and columns with gap penalties
         for i in range(1, m+1):
-            alignment_matrix[i][0] = i * self.gap
+            scoring_matrix[i][0] = i * self.gap
             path_matrix[i][0] = 1 # Backtrack left (traveled right)
         for j in range(1, n+1):
-            alignment_matrix[0][j] = j * self.gap
+            scoring_matrix[0][j] = j * self.gap
             path_matrix[0][j] = 2 # Backtrack up (traveled down)
+        return scoring_matrix, path_matrix
 
+
+    def needleman_algo(self, seq1, seq2):
         # Assign scores at each position in the matrix for possible movements
+        scoring_matrix, path_matrix = self._initialize_matrices(seq1, seq2)
+        m, n = len(seq1), len(seq2)
+
         for i in range(1, m+1):
-            for j in range(1, j+1):
-                diagonal_score = alignment_matrix[i-1][j-1] + self.match
-                down_score = alignment_matrix[i-1][j] + self.gap
-                right_score = alignment_matrix[i][j-1] + self.gap
+            for j in range(1, n+1):
+                diagonal_score = scoring_matrix[i-1][j-1] + (self.match if seq1[i-1] == seq2[j-1] else self.mismatch)
+                down_score = scoring_matrix[i-1][j] + self.gap
+                right_score = scoring_matrix[i][j-1] + self.gap
                 score = max(diagonal_score, right_score, down_score)
-                alignment_matrix[i][j] = score
+                scoring_matrix[i][j] = score
 
                 # Populate path matrix for backtracking based on travel score
                 if score == diagonal_score:
@@ -101,5 +106,6 @@ class Align:
                     path_matrix[i][j] = 1
 
         self.path_matrix = path_matrix
+        self.scoring_matrix = scoring_matrix
 
-        return alignment_matrix, path_matrix
+        return scoring_matrix, path_matrix

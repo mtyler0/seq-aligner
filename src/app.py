e48cd619cadc8363148d4a27d546f80347ea159d
Michael <mtyler745@gmail.com>
2025-09-16 01:48:46 -0400

    Improving backend with post-redirect-get functionality for scalability

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
 from core.utils import *
 from core.needleman_wunsch import *
 from core.smith_waterman import *
-from flask import Flask, render_template, request, redirect, g
+from flask import Flask, render_template, request, redirect, g, flash, url_for
 import sqlite3
 
 
 app = Flask(__name__)
+app.secret_key = "2nf38-f2n398"
 DATABASE = "data\\saved_jobs.db"
 
 def get_db():
@@ -28,51 +29,68 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/submit_form", methods=["GET", "POST"])
+@app.route("/submit_form", methods=["POST"])
+def post_form():
+    match = request.form.get("match", type=int)
+    mismatch = request.form.get("mismatch", type=int)
+    gap = request.form.get("gap", type=int)
+    seq1_file = request.files.get("seq1file")
+    seq2_file = request.files.get("seq2file")
+    print(seq1_file, seq2_file)
+
+    params = main(match, mismatch, gap)#, seq1_file, seq2_file)
+
+    db = get_db()
+    cursor = db.cursor() # type: ignore
+    try:
+        cursor.execute(
+            """
+            INSERT INTO jobs (
+            match_score,
+            mismatch,
+            gap,
+            nw_seq1_name, 
+            nw_seq2_name, 
+            nw_result, 
+            sw_seq1_name, 
+            sw_seq2_name, 
+            sw_result
+            )
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """, (match, mismatch, gap, *params)
+            )
+        db.commit() # type: ignore
+        job_id = cursor.lastrowid
+        flash("Submitting alignment job...")
+        return redirect(url_for("submit", job_id=job_id))
+
+    except Exception as e:
+        db.rollback() # type: ignore
+        print("DB insert failed:", e)
+        return redirect(url_for("index"))
+
+
+@app.route("/submit")
 def submit():
-    if request.method == "POST":
-        match = request.form.get("match", type=int)
-        mismatch = request.form.get("mismatch", type=int)
-        gap = request.form.get("gap", type=int)
-        seq1_file = request.form.get("seq1file")
-        seq2_file = request.form.get("seq2file")
-        print(seq1_file, seq2_file)
+    job_id = request.args.get("job_id")
 
-        nw_seq1, nw_seq2, nw_alignment, sw_seq1, sw_seq2, sw_alignment = params = main(match, mismatch, gap)
+    db = get_db()
+    cursor = db.cursor() # type: ignore
+    cursor.execute("SELECT * FROM jobs WHERE rowid = ?", (job_id,))
+    job = cursor.fetchone()
 
-        db = get_db()
-        cursor = db.cursor() # type: ignore
-        try:
-            cursor.execute(
-                """
-                INSERT INTO jobs (
-                nw_seq1_name, 
-                nw_seq2_name, 
-                nw_result, 
-                sw_seq1_name, 
-                sw_seq2_name, 
-                sw_result
-                ) 
-                VALUES (?, ?, ?, ?, ?, ?)
-                """, params
-                )
-            cursor.execute("INSERT INTO jobs (match_score, mismatch, gap) VALUES (?, ?, ?)", (match, mismatch, gap))
-            db.commit() # type: ignore
-            job_id = cursor.lastrowid
+    if job is None:
+        flash("Job not found.", "error")
+        return redirect(url_for("index"))
 
-        except Exception as e:
-            db.rollback() # type: ignore
-            print("DB insert failed:", e)
+    return render_template("submit_form.html", 
+        nw_alignment=job["nw_result"],
+        nw_seq1=job["nw_seq1_name"],
+        nw_seq2=job["nw_seq2_name"],
+        sw_alignment=job["sw_result"],
+        sw_seq1=job["sw_seq1_name"],
+        sw_seq2=job["sw_seq2_name"])
 
-    return render_template(
-        "submit_form.html",
-        nw_alignment=nw_alignment,
-        nw_seq1=nw_seq1,
-        nw_seq2=nw_seq2,
-        sw_alignment=sw_alignment,
-        sw_seq1=sw_seq1,
-        sw_seq2=sw_seq2,
-    )
 
 @app.route("/jobs")
 def get_jobs():
@@ -83,18 +101,15 @@ def get_jobs():
     return render_template("jobs.html",results=results)
 
 
-def main(match, mismatch, gap):
+def main(match, mismatch, gap):#, sequence1_name, sequence2_name):
     sequence1_name, sequence1, is_multiseq = get_fasta_seq("data\\examples\\seq1.fasta") # DNA1: data\\seq1.fasta, Protein: data\\human_hbb.fasta
     sequence2_name, sequence2, is_multiseq = get_fasta_seq("data\\examples\\seq2.fasta") # DNA2: data\\seq2.fasta, Protein: data\\puffer_hbb.fasta
     blosum62 = get_aa_matrix("resources\\blosum62.txt")
     a = AlignNW("dna", aa_matrix=None, match=match, mismatch=mismatch, gap=gap)
     b = AlignSW("dna", aa_matrix=None, match=match, mismatch=mismatch, gap=gap)
 
-    #print(a.get_alignment(sequence1, sequence2))
-    #print(b.get_alignment(sequence1, sequence2))
-    #print(sequence1_name, sequence1, sequence2_name, sequence2)
-
-    return sequence1_name, sequence2_name, a.get_alignment(sequence1, sequence2), sequence1_name, sequence2_name, b.get_alignment(sequence1, sequence2)
+    return sequence1_name, sequence2_name, a.get_alignment(sequence1, sequence2), \
+            sequence1_name, sequence2_name, b.get_alignment(sequence1, sequence2)
 
 
 if __name__ == "__main__":

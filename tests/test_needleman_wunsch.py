43c99d7e038a7d9e60be05d8e1c0798ba9368a92
Michael Tyler <mtyler745@gmail.com>
2025-10-09 20:39:49 -0400

    Started to add unit testing

--- tests/test_needleman_wunsch.py
+++ tests/test_needleman_wunsch.py
@@ -1,23 +1,12 @@
-import os
+from src.core import needleman_wunsch as nw, smith_waterman as sw
 from src.core import utils
 
-# a = needleman_wunsch.AlignNW("dna", aa_matrix=None)
-# b = needleman_wunsch.AlignNW("dna", aa_matrix=None)
+def test_alignmentNW():
+    n = nw.AlignNW("DNA", match=1, mismatch=-1, gap=-2)
+    result = n.get_alignment("GATTCA","GACTGU")
+    assert result[1] > 0
+    assert "G" in result[0][0]
+    print(result[1], result[0][0])
 
-data_file = os.path.join(os.path.dirname(__file__), "..", "src", "data", "examples", "fw seqs r4.docx")
-data_file = os.path.abspath(data_file)
-
-def test_get_alignmentNW():
-    pass
-
-def test_get_alignmentSW():
-    pass
-
-print(utils.get_fasta_seq(data_file))
-
-# import chardet
-
-# with open(data_file, "rb") as f:
-#     raw_data = f.read()
-#     result = chardet.detect(raw_data)
-#     print(result['encoding'])
+if __name__ == "__main__":
+    test_alignmentNW()

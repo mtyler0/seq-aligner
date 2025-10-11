781d8efae0938122c79f19f346ddad49ab944596
Michael Tyler <mtyler745@gmail.com>
2025-10-10 20:44:00 -0400

    Adding more unit tests

--- tests/test_align.py
+++ tests/test_align.py
@@ -1,15 +1,20 @@
 from src.core import needleman_wunsch as nw
 from src.core import smith_waterman as sw
-from src.core import utils
+from src.core.utils import *
 
 
 def test_alignmentNW():
     n = nw.AlignNW("DNA", None, match=1, mismatch=-1, gap=-2)
     result = n.get_alignment("GATTCA","GACTGU")
-    # assert result[1] > 0
-    # assert "G" in result[0][0]
-    print(result)
+    score = result[1]
+    assert score == 0
+    
+    
+def test_fasta_parsing():
+    seq_name, seq = get_fasta_seq("seq1.fasta")
+    assert seq_name == ">Sequence1:ABCD1"
+    assert seq == "AAAAATTTCTCGGCGTCCGCGCTAA"
 
-
+    
 if __name__ == "__main__":
     test_alignmentNW()
